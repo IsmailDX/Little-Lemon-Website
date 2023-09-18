045f2ed9a8be455c92906126ddc83478e93944e7
IsmailDX <ismaildxx@gmail.com>
2023-09-18 19:44:52 +0400

    first commit

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./HomePage.css";
+import Food from "../assets/Rectangle 45.png";
+
+const HomePage = () => {
+  return (
+    <div className="section">
+      <div className="content">
+        <div className="col1">
+          <h1>Little Lemon</h1>
+          <h4>Chicago</h4>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque, eos
+            tempore. Nam ut autem eaque corrupti repudiandae quae rem modi!
+          </p>
+          <button id="button1">Reserve a table</button>
+        </div>
+        <div className="imgContainer">
+          <img className="food" src={Food} alt="food" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import Logo from "../assets/logo1.png";
+import Shop from "../assets/iconshop.png";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
+const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleClick = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div>
+      <nav>
+        <div className="navbarContainer">
+          {nav ? (
+            <ul className="ulSmall">
+              <div className="sideBar">
+                <img src={Logo} className="Littlelemon2" alt="logo1" />
+                <AiOutlineClose
+                  className="close"
+                  size={30}
+                  onClick={handleClick}
+                />
+              </div>
+              <li>
+                <Link to="/home" className="nav-item">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/home" className="nav-item">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link to="/home" className="nav-item">
+                  Menu
+                </Link>
+              </li>
+              <li>
+                <Link to="/home" className="nav-item">
+                  Reservations
+                </Link>
+              </li>
+              <li>
+                <Link to="/home" className="nav-item">
+                  Order Online
+                </Link>
+              </li>
+              <li>
+                <Link to="/home" className="nav-item">
+                  Login
+                </Link>
+              </li>
+            </ul>
+          ) : null}
+
+          <AiOutlineMenu className="menu" size={30} onClick={handleClick} />
+          <img src={Logo} className="Littlelemon" alt="logo1" />
+          <img src={Shop} className="shopIcon" alt="shop" />
+          <ul className="ulBig">
+            <li>
+              <Link to="/home" className="nav-item">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/home" className="nav-item">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/home" className="nav-item">
+                Menu
+              </Link>
+            </li>
+            <li>
+              <Link to="/home" className="nav-item">
+                Reservations
+              </Link>
+            </li>
+            <li>
+              <Link to="/home" className="nav-item">
+                Order Online
+              </Link>
+            </li>
+            <li>
+              <Link to="/home" className="nav-item">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
