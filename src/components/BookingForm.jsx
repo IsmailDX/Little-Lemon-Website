79f88515dca17c054a3a3603df613f091863a17f
IsmailDX <ismaildxx@gmail.com>
2023-09-25 00:31:53 +0400

    used formik on form and did form validation

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,4 +1,7 @@
 import './styles/Form.css'
+import React from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 import { useState, useEffect } from 'react'
 import { fetchAPI } from './Api'
 
@@ -23,67 +26,126 @@ const BookingForm = ({
         }
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        onReservationComplete()
-    }
+    const formik = useFormik({
+        initialValues: {
+            dateField: '',
+            timeField: '',
+            guestsField: '',
+            occasionField: '',
+        },
+        validationSchema: Yup.object({
+            dateField: Yup.string().required('Required'),
+            timeField: Yup.string().required('Required'),
+            guestsField: Yup.string().required('Required').max(10),
+            occasionField: Yup.string().required('Required'),
+        }),
+        onSubmit: (values) => {
+            onReservationComplete()
+        },
+    })
+
+    useEffect(() => {
+        if (formik.values.dateField) {
+            fetchAPI(formik.values.dateField)
+        }
+    }, [formik.values.dateField, updateTimes])
 
     return (
-        <form>
+        <form onSubmit={formik.handleSubmit}>
             <h1 id="bookh1">Booking Form</h1>
+
             <label htmlFor="res-date">Choose date</label>
-            <input
-                type="date"
-                id="res-date"
-                onChange={(e) => {
-                    handleInputChange('date', e.target.value)
-                }}
-            />
+            <div className="ChooseDiv">
+                <input
+                    type="date"
+                    id="res-date"
+                    name="dateField"
+                    onChange={(e) => {
+                        handleInputChange('date', e.target.value)
+                        formik.handleChange(e)
+                    }}
+                />
+                {formik.errors.dateField ? (
+                    <div className="error-message">
+                        {formik.errors.dateField}
+                    </div>
+                ) : null}
+            </div>
 
             <label htmlFor="res-time">Choose time</label>
-            <select
-                id="res-time"
-                onChange={(e) => {
-                    handleInputChange('time', e.target.value)
-                }}
-            >
-                {availableTimes.map((option) => (
-                    <option key={option} value={option}>
-                        {option}
+            <div className="ChooseDiv">
+                <select
+                    id="res-time"
+                    name="timeField"
+                    onChange={(e) => {
+                        handleInputChange('time', e.target.value)
+                        formik.handleChange(e)
+                    }}
+                >
+                    <option disabled selected value="">
+                        Select Time
                     </option>
-                ))}
-            </select>
+                    {availableTimes.map((option) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </select>
+                {formik.errors.timeField ? (
+                    <div className="error-message">
+                        {formik.errors.timeField}
+                    </div>
+                ) : null}
+            </div>
 
             <label htmlFor="guests">Number of Guests</label>
-            <input
-                type="number"
-                placeholder="1"
-                min="1"
-                max="10"
-                id="guests"
-                onChange={(e) => {
-                    handleInputChange('numOfGuests', e.target.value)
-                }}
-            />
+            <div className="ChooseDiv">
+                <input
+                    type="number"
+                    placeholder="Number of guests"
+                    min="1"
+                    max="10"
+                    id="guests"
+                    name="guestsField"
+                    onChange={(e) => {
+                        handleInputChange('numOfGuests', e.target.value)
+                        formik.handleChange(e)
+                    }}
+                />
+                {formik.errors.guestsField ? (
+                    <div className="error-message">
+                        {formik.errors.guestsField}
+                    </div>
+                ) : null}
+            </div>
 
             <label htmlFor="occasion">Select Occasion</label>
-            <select
-                id="occasion"
-                onChange={(e) => {
-                    handleInputChange('occasion', e.target.value)
-                }}
-            >
-                <option disabled selected value="">
-                    Select Occasion
-                </option>
-                <option value="Birthday">Birthday</option>
-                <option value="Anniversary">Anniversary</option>
-            </select>
+            <div className="ChooseDiv">
+                <select
+                    id="occasion"
+                    name="occasionField"
+                    onChange={(e) => {
+                        handleInputChange('occasion', e.target.value)
+                        formik.handleChange(e)
+                    }}
+                >
+                    <option disabled selected value="">
+                        Select Occasion
+                    </option>
+                    <option value="Birthday">Birthday</option>
+                    <option value="Anniversary">Anniversary</option>
+                </select>
+                {formik.errors.occasionField ? (
+                    <div className="error-message">
+                        {formik.errors.occasionField}
+                    </div>
+                ) : null}
+            </div>
             <input
                 type="submit"
                 value="Make Your Reservation"
                 id="submitButton"
-                onClick={handleSubmit}
+                disabled={!formik.isValid}
             />
         </form>
     )
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,42 +1,42 @@
-import React from "react";
-import "./styles/Testimonials.css";
-import Me from "../assets/Me.jpg";
-import Box from "./Box";
-import Basha from "../assets/basha.jpg";
-import Thomas from "../assets/Ismaildx.png";
-import Rash from "../assets/rash.png";
-import Font, { Text } from "react-font";
+import React from 'react'
+import './styles/Testimonials.css'
+import Me from '../assets/Me.jpg'
+import Box from './Box'
+import Basha from '../assets/basha.jpg'
+import Thomas from '../assets/Ismaildx.png'
+import Rash from '../assets/rash.png'
+import Font from 'react-font'
 
 const Testimonials = () => {
-  return (
-    <div className="section3">
-      <Font family="Markazi Text">
-        <h3 className="section3-title">Testimonials</h3>
-      </Font>
-      <div className="section-container">
-        <Box
-          Name="Ismail Hussein"
-          photo={Me}
-          bottomtext="Lorem ipsum dolor sit."
-        />
-        <Box
-          Name="Thomas Shelby"
-          photo={Thomas}
-          bottomtext="Lorem ipsum dolor sit."
-        />
-        <Box
-          Name="Talaat Harb"
-          photo={Basha}
-          bottomtext="Lorem ipsum dolor sit."
-        />
-        <Box
-          Name="Marcus Rashford"
-          photo={Rash}
-          bottomtext="Lorem ipsum dolor sit."
-        />
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className="section3">
+            <Font family="Markazi Text">
+                <h3 className="section3-title">Testimonials</h3>
+            </Font>
+            <div className="section-container">
+                <Box
+                    Name="Ismail Hussein"
+                    photo={Me}
+                    bottomtext="Lorem ipsum dolor sit."
+                />
+                <Box
+                    Name="Thomas Shelby"
+                    photo={Thomas}
+                    bottomtext="Lorem ipsum dolor sit."
+                />
+                <Box
+                    Name="Talaat Harb"
+                    photo={Basha}
+                    bottomtext="Lorem ipsum dolor sit."
+                />
+                <Box
+                    Name="Marcus Rashford"
+                    photo={Rash}
+                    bottomtext="Lorem ipsum dolor sit."
+                />
+            </div>
+        </div>
+    )
+}
 
-export default Testimonials;
+export default Testimonials
